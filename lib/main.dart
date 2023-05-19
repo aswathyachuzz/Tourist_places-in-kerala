e16d7716be203b31ecd4c7e99f796fe11c0599ca
aswathyachuzz <80675919+aswathyachuzz@users.noreply.github.com>
2023-05-19 13:00:10 +0530

    Kerala Tourist Places

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(249, 8, 8, 0.537),
+              Color.fromRGBO(80, 8, 249, 0.628),
+              Color.fromRGBO(11, 203, 155, 0.627)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello Pappuu',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
